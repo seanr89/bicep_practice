e571c17c6815c5b5d86611ef9f2bb300572a3a94
seanr89 <srafferty89@gmail.com>
2024-04-07 11:59:48 +0100

    Add Azure Function deployment scripts and configuration

--- src/Functions/run.sh
+++ src/Functions/run.sh
@@ -2,5 +2,14 @@
 
 echo "Function Creator"
 
+if [[ ! -n $1 ]];
+then 
+    echo "No parameter passed."
+else
+    echo "Parameter passed = $1"
+fi
+
+az deployment group create $1 --resource-group bibsRG --template-file function.bicep
+# az deployment group create --resource-group bibsRG --template-file function.bicep --parameters appInsightsLocation=<app-location>
 
 echo 'Run complete!'
